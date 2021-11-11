2d0efe6191940c3268c0c8ad273a88440e8d4ef9
Shri Khare <khare@exploremetakinetic.com>
2021-11-11 16:31:18 -0500

    assertEqual completed

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected){
+  if(actual === expected){
+    return console.log('Assertion Passed: ' + actual + ' === ' + expected)
+  } else {
+    return console.log('Assertion Failed: "' + actual + '" !== "' + expected + '"')
+  }
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1,1)
