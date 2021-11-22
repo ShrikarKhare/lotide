57f136decba91785d48363c28527632e37d59ffb
Shri Khare <khare@exploremetakinetic.com>
2021-11-22 12:25:12 -0500

    added chai and mocha, refactored headTest and middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]); //Test Case
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]),[4,5]); //Test Case
-assertArraysEqual(middle([1,2,3,4,5,6,7,99,21635,2,6,48]),[6,7]); //Test Case
-assertArraysEqual(middle([1,2,3,4,5,6,7,8,1,1,1,1,1,1,1,1,2]),[8]); //Test Case
+
+describe('#middle', () => {
+    it('returns 4 for [1,2,3,4,5,6,7]', () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7]), 4);
+    });
+    it('it returns [4,5] for [1,2,3,4,5,6,7,8]', () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+    });
+    it('it returns 1 for [1,2,3,4,5,6,7,8,1,1,1,1,1,1,1,1,2]', () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7,8,1,1,1,1,1,1,1,1,2]),1);
+    });
+    it('it returns [6,7] for [1,2,3,4,5,6,7,99,21635,2,6,48]', () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7,99,21635,2,6,48]), [6,7]);
+    });
+});
