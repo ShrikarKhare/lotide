d556a663da444f0809e810d14cd3c1ff67485524
Shri Khare <khare@exploremetakinetic.com>
2021-11-23 23:36:38 -0500

    saving unit test work

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
 const head = require('./head');
+const map = require('./map');
 const middle = require('./middle');
-
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 module.exports = {
+    assertArraysEqual : assertArraysEqual,
+    assertEqual : assertEqual,
+    assertObjectsEqual : assertObjectsEqual,
+    countOnly : countOnly,
+    eqArrays : eqArrays,
+    eqObjects : eqObjects,
+    findKey : findKey,
+    findKeyByValue : findKeyByValue,
     head : head,
+    map : map,
     middle : middle,
+    takeUntil : takeUntil,
+    without : without,
 };
